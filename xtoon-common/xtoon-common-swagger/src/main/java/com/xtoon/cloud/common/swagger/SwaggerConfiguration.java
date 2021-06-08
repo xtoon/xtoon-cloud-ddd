febd1d087a6dd6dd347c94b5b96d54c0e553aa23
haoxin963 <525899665@qq.com>
2021-06-08 22:22:30 +0800

    通用模块开发

---
+++ xtoon-common/xtoon-common-swagger/src/main/java/com/xtoon/cloud/common/swagger/SwaggerConfiguration.java
@@ -0,0 +1,88 @@
+package com.xtoon.cloud.common.swagger;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.models.auth.In;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.RequestParameterBuilder;
+import springfox.documentation.schema.ScalarType;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static com.xtoon.cloud.common.core.constant.CommonConstant.AUTH_KEY;
+
+/**
+ * Swagger配置
+ *
+ * @author haoxin
+ * @date 2021-06-08
+ **/
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build()
+                .securitySchemes(security())
+                .securityContexts(securityContexts())
+                .globalRequestParameters(getGlobalRequestParameters());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("xtoon-cloud")
+                .description("xtoon-cloud是一款基于DDD领域模型设计并支持SaaS平台的微服务框架")
+                .termsOfServiceUrl("http://www.xiangtoon.com")
+                .contact(new Contact("享同科技", "https://blog.csdn.net/haoxin963", "525899665@qq.com"))
+                .version("v1.0.0")
+                .build();
+    }
+
+    private List<SecurityScheme> security() {
+        ApiKey apiKey = new ApiKey("token", "token", In.HEADER.toValue());
+        return Collections.singletonList(apiKey);
+    }
+
+    /**
+     * 授权信息全局应用
+     */
+    private List<SecurityContext> securityContexts() {
+        return Collections.singletonList(
+                SecurityContext.builder()
+                        .securityReferences(Collections.singletonList(new SecurityReference(AUTH_KEY, new AuthorizationScope[]{new AuthorizationScope("global", "")})))
+                        .build()
+        );
+    }
+
+    /**
+     * 生成全局通用参数
+     *
+     * @return
+     */
+    private List<RequestParameter> getGlobalRequestParameters() {
+        List<RequestParameter> parameters = new ArrayList<>();
+        parameters.add(new RequestParameterBuilder()
+                .name("tenant_id")
+                .description("租户id")
+                .required(true)
+                .in(ParameterType.HEADER)
+                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
+                .required(false)
+                .build());
+        return parameters;
+    }
+}
